2ba34c90da730ef42f0a70b28dd42dfe9799561d
EdsonRosas3 <edsonrosas321@gmail.com>
2021-04-14 20:57:44 -0400

    se implemento la peticion getQuotitation

--- src/pages/solictudesVista/SolicitudesVista.js
+++ src/pages/solictudesVista/SolicitudesVista.js
@@ -7,11 +7,10 @@ function SolicitudesVista(){
     useEffect(() => {
         async function getAllQuotitations() {
             const result = await getQuotitation();
-            setQuotitations(result.request_quotitations);
-            
+            const resultQuotitations=result.request_quotitations;
+            setQuotitations(resultQuotitations);
         }
         getAllQuotitations();
-        console.log(quotitations)
         //eslint-disable-next-line
     }, []);
     return(
@@ -32,75 +31,30 @@ function SolicitudesVista(){
                         <table className="table table-striped">
                             <thead>
                                 <tr>
-                                <th className="col-1">#</th>
-                                <th className="col-2">Unidad de Gasto</th>
-                                <th className="col-2">Fecha</th>
-                                <th className="col-2">solicitud</th>
-                                <th className="col-3">Estado</th>
-                                <th className="col-2">Informe</th>
+                                    <th className="col-1">#</th>
+                                    <th className="col-2">Unidad de Gasto</th>
+                                    <th className="col-2">Fecha</th>
+                                    <th className="col-2">solicitud</th>
+                                    <th className="col-3">Estado</th>
+                                    <th className="col-2">Informe</th>
                                 </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td className="col-1">1</td>
-                                <td className="col-2">limpieza</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-2">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
-                            <tr>
-                                <td className="col-1">2</td>
-                                <td className="col-2">maquinaria</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-2">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
-                            <tr>
-                                <td className="col-1">3</td>
-                                <td className="col-2">laboratorios</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-2">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
-                            <tr>
-                                <td className="col-1">1</td>
-                                <td className="col-2">limpieza</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-sm-2" align="char">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
-                            <tr>
-                                <td className="col-1">2</td>
-                                <td className="col-2">maquinaria</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-2">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
-                            <tr>
-                                <td className="col-1">3</td>
-                                <td className="col-2">laboratorios</td>
-                                <td className="col-2">02/02/21</td>
-                                <td className="col-2">
-                                    <a className="link">ver</a>
-                                </td>
-                                <td className="col-3">pendiente</td>
-                                <td className="col-2">----</td>
-                            </tr>
+                            {quotitations.map((quotitation) => {
+                                return(
+                                    <tr key={quotitation.id}>
+                                    <td className="col-1">{quotitation.id}</td>
+                                    <td className="col-2">{quotitation.nameUnidadGasto}</td>
+                                    <td className="col-2">{quotitation.requestDate}</td>
+                                    <td className="col-2">
+                                        <a className="link">ver</a>
+                                    </td>
+                                    <td className="col-3">{quotitation.status}</td>
+                                    <td className="col-2">----</td>
+                                </tr>
+                                );
+                            })}
+                                
                             </tbody>
                         </table>
                     </div>
