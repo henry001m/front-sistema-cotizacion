fc2513c98e77a2917408317da43082b061ce777c
henry001m <henrymontano700@gmail.com>
2021-05-14 23:27:36 -0400

    prueba de la ventana ver archivo

--- src/pages/verArchivos/VentanaVerArchivo.js
+++ src/pages/verArchivos/VentanaVerArchivo.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import FileViewer from 'react-file-viewer'
 function VentanaVerArchivo(){
+
+    const file = 'localhost:8000/api/showFile/1/055625-20201223_123846.pdf'
+    const type = 'pdf'
+
+    const onError = (e) => {
+        console.log(e, 'error in file-viewer');
+    }
 
     return(
         <div>
-            <iframe src="https://docs.google.com/viewer?url=http://localhost:8000/api/showFile/1/055625-20201223_123846.pdf&embedded=true" width='100%' height='600px' frameborder='0'>
-            </iframe>
+            <FileViewer
+            fileType={type}
+            filePath={file}
+            onError={onError}/>
         </div>
     );
 }
