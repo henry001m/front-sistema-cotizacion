3fc17c208a02b2ac995d1f12d4a379e0d33190d9
henry001m <henrymontano700@gmail.com>
2021-05-15 00:13:55 -0400

    vista ver archivo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { BrowserRouter as Router} from 'react-router-dom'
 import Navbar from './components/navbar/Navbar';
 import Main from './pages/Main';
+import VentanaVerArchivo from './pages/verArchivos/VentanaVerArchivo';
 
 function App() {
   return (
     <Router>
-        <Navbar/>
-        <Main/>
+        {/*<Navbar/>
+        <Main/>*/}
+        <VentanaVerArchivo></VentanaVerArchivo>
     </Router>
   );
 }
--- src/pages/verArchivos/VentanaVerArchivo.js
+++ src/pages/verArchivos/VentanaVerArchivo.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import FileViewer from 'react-file-viewer'
-function VentanaVerArchivo(){
 
-    const file = 'localhost:8000/api/showFile/1/055625-20201223_123846.pdf'
-    const type = 'pdf'
+const file = 'http://localhost:8000/api/showFile/1/055625-20201223_123846.pdf'
+const type = 'pdf'
+
+function VentanaVerArchivo(){
 
     const onError = (e) => {
         console.log(e, 'error in file-viewer');
