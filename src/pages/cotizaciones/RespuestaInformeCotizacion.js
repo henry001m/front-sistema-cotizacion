a161157a9be43cc059ca400494b91ae929d32b47
henry001m <henrymontano700@gmail.com>
2021-06-19 22:01:06 -0400

    cuadro comparativo de ver respuesta de cotizacion (informe)

--- src/pages/cotizaciones/RespuestaInformeCotizacion.js
+++ src/pages/cotizaciones/RespuestaInformeCotizacion.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function RespuestaInformeCotizacion(){
 
+    const [ solicitud, setSolicitud ] = useState({
+        items:[
+            {
+                id:1, descripcion:"barbijos", amount:20,
+                cotizaciones:[
+                    {empresa:"chicken", total:200},{empresa:"Sensacion", total:350},{empresa:"Keido", total:400}
+                ]
+            },
+            {
+                id:2, descripcion:"guantes", amount:25,
+                cotizaciones:[
+                    {empresa:"chicken", total:100},{empresa:"Sensacion", total:420},{empresa:"Keido", total:null}
+                ]
+            },
+            {
+                id:3, descripcion:"lentes", amount:30,
+                cotizaciones:[
+                    {empresa:"chicken", total:250},{empresa:"Sensacion", total:null},{empresa:"Keido", total:300}
+                ]
+            },
+        ]
+    })
+
     const closePage = () => {
 
+    }
+
+    const ValorMenor = ( lista ) => {
+
+        var menor = lista[0].total;
+
+        lista.forEach(element => {
+            if(element.total<menor && element.total!=null){
+                menor = element.total;
+            }
+        });
+        
+        return(
+            <td>{menor}</td>
+        )
+    }
+
+    const SumaTotal = ( index ) => {
+        var suma = 0;
+        solicitud.items.forEach(element => {
+            if(element.cotizaciones[index].total!=null){
+                suma=suma+element.cotizaciones[index].total;
+            }
+        });
+
+        return(
+            <td>{suma}</td>
+        );
     }
 
     return(
@@ -45,16 +96,40 @@ function RespuestaInformeCotizacion(){
                                         <th scope="col">#</th>
                                         <th scope="col">Producto</th>
                                         <th scope="col">Cantidad</th>
-                                        <th scope="col">Empresa 1</th>
-                                        <th scope="col">Empresa 1</th>
-                                        <th scope="col">Empresa 1</th>
-                                        <th scope="col">Empresa 1</th>
-                                        <th scope="col">Empresa 1</th>
+                                        {
+                                            solicitud.items[0].cotizaciones.map((empresa,index)=>(
+                                                <th scope="col">{empresa.empresa}</th>
+                                            ))
+                                        }
                                         <th scope="col">Precio mas bajo</th>
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    
+                                    {
+                                        solicitud.items.map((item,index)=>(
+                                            <tr key={item.id}>
+                                                <th scope="row">{index+1}</th>
+                                                <td >{item.descripcion}</td>
+                                                <td>{item.amount}</td>
+                                                {
+                                                    item.cotizaciones.map((cotizacion,index)=>(
+                                                        <td key={index}>{cotizacion.total}</td>
+                                                    ))
+                                                }
+                                                {
+                                                    ValorMenor(item.cotizaciones)
+                                                }
+                                            </tr>
+                                        ))
+                                    }
+                                    <tr>
+                                        <th scope="row"></th>
+                                        <td>Total</td>
+                                        <td></td>
+                                        {
+                                            solicitud.items[0].cotizaciones.map((cotizacion,index)=> SumaTotal(index))
+                                        }
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
