0b36a234e35490a2e1ecedab3a7985b6d0ba757a
EdsonRosas3 <edsonrosas321@gmail.com>
2021-07-07 22:47:00 -0400

    nueva actualizacion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { Fragment,useState,useEffect } from 'react'
+import React, { useState } from 'react';
+import logoUmss from '../assets/logoumss.png';
 function Home(){
     const [user, setUser] = useState(null);
     return(
@@ -7,13 +8,13 @@ function Home(){
         {(user == null) ? (
             <div className="text-center">
             <h1>Bienvenido</h1>
-            <img src="./logoumss.png"></img>
+            <img src={logoUmss}></img>
             </div>
         ):(
             <div className="text-center">
             <h1>Bienvenido al sistema</h1>
             <h2>Revise sus datos personales</h2>
-            <img src="./logoumss.png"></img>
+            <img src={logoUmss}></img>
             </div>
         )
        }
